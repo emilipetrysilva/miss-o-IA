7bb075ebf118d925f9fb113e5457a949f4cbffad
Emili Petry da Silva <112400414+emilipetrysilva@users.noreply.github.com>
2024-11-05 08:00:01 -0300

    Add files via upload

---
+++ missão IA/js/perguntas.js
@@ -0,0 +1,41 @@
+export const perguntas = [
+    {
+        enunciado: "A evolução da IA pode ajudar a sociedade, assim como pode afeta-la. Para você, a IA pode gerar mais riscos ou benefícios  a sociedade?",
+        alternativas: [
+            {
+                texto:"Beneficios",
+                afirmacao: "Mercado de trabalho, saúde, segurança, educação, políticas públicas, energia, biodiversidade e atendimento ao cliente."
+            },
+            {
+                texto: "Maleficios",
+                afirmacao: "Violação de privacidade, perpetuação de preconceitos, desemprego, falta de empatia, desigualdade de acesso, riscos a saúde e pornografia infantil."
+            }
+        ]
+    },
+    {
+        enunciado: "Um possível colapso social causado por IA te assusta?",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "a probabilidade de um colapso global realmente é assustadora, podendo assim destruir a sociedade como um todo."
+            },
+            {
+                texto: "não",
+                afirmacao: "Acreditar na prevenção de um colapso é a forma de não temer o próprio."
+            }
+        ]
+    },
+    {
+        enunciado: "Você acredita que os humanos estão se prevenindo de uma possível evolução exponencial das IAs?",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "com a evolução da mente humana podemos sim nos prevenir do colapso."
+            },
+            {
+                texto: "não",
+                afirmacao: "Com a evolução exponencial seria de extrema dificuldade para nós humanos nos prevenirmos de um provável colapso social"
+            }
+        ]
+    },
+];
